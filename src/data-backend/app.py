38dc8be077dd351199b42aa4f61f57e900b6ad1e
Krish <krishmakkar11@gmail.com>
2025-07-25 22:09:39 +0100

    Finished API for csv breakdown (I think), including dockerfile

---
+++ src/data-backend/app.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import base64
+import io
+
+def parse_contents(contents, filename):
+    _, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            df = pd.read_excel(io.BytesIO(decoded))
+        return df
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An internal error occurred: {e}")
+
+class CsvData(BaseModel):
+    contents: str
+    filename: str
+    skiprows: int
+
+app = FastAPI(title='Backend csv Analysis API')
+
+@app.post("/process-csv/")
+def obtain_time_and_volt(request: CsvData):
+    try:
+        df = parse_contents(request.contents, request.filename)
+        skip_index = request.skiprows
+        time = df.iloc[:, skip_index:]
+        volt = df.iloc[:, skip_index:]
+        return {
+            "time": time.tolist(),
+            "volt": volt.tolist(),
+            "Response":'file analysed successfully' 
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An internal error occurred: {e}")
